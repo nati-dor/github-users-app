a2b16ece9ec0a3f9f8fcb967a07735092be3047f
nati-dor <netanel.duk@gmail.com>
2021-08-11 18:30:23 +0300

    add style add input

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import UserInfo from "../UserInfo/UserInfo"
 import axios from "../../../node_modules/axios/dist/axios.js"
 import "./Container.css" 
+import {InputGroup,FormControl,Button} from "react-bootstrap"
+
 
 
 class Container extends React.Component {
@@ -9,30 +11,68 @@ class Container extends React.Component {
       super()
       this.state = {
         usersArr: ["ShaniBel","Nati-Dor","odeliamelloul", "Sparco7", "nitsan1412","leib-ran","Fareska","Harduf-l","shaharbest"],
-        infoArrUsers: []
+        infoArrUsers: [],
+        newUser: ""
       }
     }
     componentDidMount= () => {
-
-        for (let i = 0; i < this.state.usersArr.length; i++) {
-            const response = axios
-            .get(`https://api.github.com/users/${this.state.usersArr[i]}`)
-            .then((response) => {
-                let arr = [...this.state.infoArrUsers]
-                let {avatar_url,login,followers,following,created_at} = response.data
-                let objUser = {avatar_url,login,followers,following,created_at}
-                arr.push(objUser)
-                this.setState({infoArrUsers:arr})
-            })
-            .catch((err) => console.log(err));
-        }
+        for (let i = 0; i < this.state.usersArr.length; i++) 
+            this.myAxios(this.state.usersArr[i])
     }
+
+    changeNewUser=(e)=>{
+        e.preventDefault()
+        const myUser = e.target.value;
+        this.setState({newUser:myUser})
+    }
+
+    addNewUser=()=>{
+        let arr=[...this.state.usersArr]
+        if(arr.find(element => element===this.state.newUser))
+        return
+        arr.push(this.state.newUser)
+        this.setState({usersArr:arr})
+        this.myAxios(this.state.newUser)
+    }
+
+    myAxios=(str)=>{
+        const response = axios
+        .get(`https://api.github.com/users/${str}`)
+        .then((response) => {
+            const arrColors=['primary','secondary','success','danger','warning','info','light','dark']
+            const numRandom=Math.floor(Math.random() * 8);
+            let arr = [...this.state.infoArrUsers]
+            let {avatar_url,login,followers,following,created_at} = response.data
+            let objUser = {avatar_url,login,followers,following,created_at,color:arrColors[numRandom], myWeb: 'https://github.com/'+login}
+            arr.push(objUser)
+            this.setState({infoArrUsers:arr})
+        })
+        .catch((err) => console.log(err));
+    }
+
+
       render () {
             return (
-                <div>
-                {this.state.infoArrUsers.map((user,index) => 
-                 <UserInfo user ={user} key= {index}/>
-                 )}
+                <div className="container">
+                    <div className="inputUser">
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">Enter User Name: </InputGroup.Text>
+                        <FormControl
+                         onChange={this.changeNewUser}
+                         placeholder="Name of user GitHub"
+                         aria-label="Username"
+                         aria-describedby="basic-addon1"
+                        />
+                        <Button variant="primary" onClick={this.addNewUser}>+</Button>
+                        </InputGroup>
+                    </div>
+                    <div className="AllUsers">
+                    {this.state.infoArrUsers.map((user,index) => 
+                    <UserInfo user ={user} key= {index}/>
+                    )}
+                    </div>
+
+                
         </div>
       )
     }
--- src/Components/UserInfo/UserInfo.jsx
+++ src/Components/UserInfo/UserInfo.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import "./UserInfo.css" 
+import {Card,Button} from "react-bootstrap"
 
 function UserInfo (userData) {
+    // const arrColors=['primary','secondary','success','danger','warning','info','light','dark']
+    // const numRandom=Math.floor(Math.random() * 8);
+    let but;
+    if(userData.user.color === 'dark')
+     but = <Button variant="outline-light">My Web</Button>
+    else
+     but= <Button variant="outline-dark" href={userData.user.myWeb}>My Web</Button>
+   
   return (
-    <div>
-       <ul>
-       <li><img src= {userData.user.avatar_url}/></li>
-      <li>Username: {userData.user.login} </li>
-      <li>Followers: {userData.user.followers} </li>
-      <li>Following: {userData.user.following} </li>
-      <li>Created at: {userData.user.created_at}</li>
-      </ul>
-    </div>
+        <Card
+        bg={userData.user.color}
+        key={userData.user.login}
+        text={userData.user.color === 'dark' ? 'light' : 'black'}
+        style={{ maxWidth: 'min-content',minWidth: '250px'}}
+        className="mb-2"
+    >
+        <Card.Header><img src= {userData.user.avatar_url}/></Card.Header>
+        <Card.Body>
+        <Card.Title><p>{userData.user.login}</p></Card.Title>
+        <Card.Text>
+       <p>Followers: {userData.user.followers}</p> 
+       <p>Following: {userData.user.following} </p> 
+       <p>Created at: {userData.user.created_at.split('T')[0]}</p> 
+        {but}
+      </Card.Text>
+        </Card.Body>
+    </Card>
      )
 }
 
